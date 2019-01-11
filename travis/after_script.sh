da234509593ed690efeed9468a719fc1fd4f6a05
Anthony Stewart <anthony.l.stewart@outlook.com>
2019-01-10 20:11:26 -0600

    Added documentation generation

--- travis/after_script.sh
+++ travis/after_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-cd build
+pushd build
 
 lcov --directory . --capture --output-file coverage.info # capture coverage info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
@@ -10,3 +10,6 @@ lcov --remove coverage.info 'test/*' --output-file coverage.info # filter out te
 lcov --list coverage.info #debug info
 
 bash <(curl -s https://codecov.io/bash) -t a80551a3-9f8b-496c-8002-2608aa375cda
+
+popd
+doxygen Doxyfile
