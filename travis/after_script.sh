d6be90f858856346611a2d0773d6763d22d8a9f8
Anthony Stewart <anthony.l.stewart@outlook.com>
2019-01-10 21:07:14 -0600

    Moved doxygen execution to before_deploy

--- travis/after_script.sh
+++ travis/after_script.sh
@@ -10,6 +10,3 @@ lcov --remove coverage.info 'test/*' --output-file coverage.info # filter out te
 lcov --list coverage.info #debug info
 
 bash <(curl -s https://codecov.io/bash) -t a80551a3-9f8b-496c-8002-2608aa375cda
-
-popd
-doxygen Doxyfile
